ea2f7438a214a18d4b0f941222dac11e76009d30
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-10-17 16:54:22 +0400

    sum-of-prime-numbers

--- src/sum_of_prime_numbers.java
+++ src/sum_of_prime_numbers.java
@@ -3,5 +3,29 @@ import java.util.Scanner;
 public class sum_of_prime_numbers {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("sayı daxil et:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter the number of array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum=0;
+        for (int i = 2; i < arr.length; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < arr[i]; j++) {
+                if (arr[i] % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                sum += arr[i];
+            }
+        }
+        System.out.println("Sum of the prime numbers: " + sum);
     }
 }
